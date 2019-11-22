440827e788fc231ca747d9226b369b112a5a17be
Hardelele <i.lapshina@mail.ru>
2019-11-22 06:16:39 +0300

    Main: change RunClass instance to StateEditor instance

--- it/src/main/java/com/github/hardelele/it/knight/Main.java
+++ it/src/main/java/com/github/hardelele/it/knight/Main.java
@@ -2,6 +2,6 @@ package com.github.hardelele.it.knight;
 
 public class Main {
     public static void main(String[] args) {
-        RunClass runClass = new RunClass();
+        StateEditor stateEditor = new StateEditor();
     }
 }
